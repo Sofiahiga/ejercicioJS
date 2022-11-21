ededff094fad52a14db02a52de790fd215aee722
Sofia Higa <sofihiga95@gmail.com>
2022-11-21 20:55:25 -0300

    ejercicios corregidos

--- Ejercicio_entregar.js
+++ Ejercicio_entregar.js
@@ -1,9 +1,29 @@
+var letras = "abcdefghijklmnñopqrstuvwxyz";
+
+function tiene_letras(texto) {
+    for(i=0; i<texto.length; i++){
+        if (letras.indexOf(texto.charAt(i))!=-1){
+            return(true);
+        }
+    }
+    return(false);
+
+}
+
 //Operaciones básicas, Tipos de datos, Prompt y Alert
 
 /*Escribir un programa que muestre un prompt donde el usuario ingresa un número, luego muestra otro prompt pidiendo otro número, finalmente el programa muestra una alerta con el resultado de la suma.*/
 
-/*let num_uno = parseInt(prompt("Ingrese un número"));
-let num_dos = parseInt(prompt("Ingrese otro número"));
+/*let num_uno =prompt("Ingrese un número");
+
+while (tiene_letras(num_uno)) {
+    num_uno= prompt("ERROR, solo debe ingresar números");
+}
+let num_dos = prompt("Ingrese otro número");
+
+while (tiene_letras(num_dos)) {
+    num_dos= prompt("ERROR, solo debe ingresar números");
+}
 
 alert(num_uno + num_dos);*/
 
@@ -16,7 +36,10 @@ alert("La temperatura en Fahrenheit es " + fahrenheit + "°F" )*/
 
 /* Pedir un número a un usuario a través de un prompt y luego dividirlo por 10, ejemplo: 5 / 10 igual 0.5 */
 
-/*let numero=parseInt(prompt("Ingrese un número"));
+/*let numero=prompt("Ingrese un número");
+while (tiene_letras(numero)) {
+    numero= prompt("ERROR, solo debe ingresar números");
+}
 let resultado=numero/10;
 
 alert(resultado);*/
@@ -30,7 +53,10 @@ alert(typeof a);*/
 
 /* Crear un programa que determine si un número introducido en un Prompt es par o no, la respuesta será mostrada en una alerta. */
 
-/*let numero=parseInt(prompt("Ingrese un numero"));
+/*let numero=prompt("Ingrese un numero");
+while (tiene_letras(numero)) {
+    numero= prompt("ERROR, solo debe ingresar números");
+}
 
 if (numero%2 == 0) {
     alert(numero + " es un numero par");
@@ -40,7 +66,10 @@ if (numero%2 == 0) {
 
 /* Crear un programa que determine si un número introducido en un Prompt es divisible por 5 o no, mostrar el resultado con console.log */
 
-/*let numero=parseInt(prompt("Ingrese un número"));
+/*let numero=prompt("Ingrese un número");
+while (tiene_letras(numero)) {
+    numero= prompt("ERROR, solo debe ingresar números");
+}
 
 if (numero%5 == 0) {
     console.log(numero + " es divisible por 5. El resultado es " + numero/5)
@@ -50,7 +79,10 @@ if (numero%5 == 0) {
 
 /* Crear un programa que determine si un número introducido en un popup es divisible por 11 y 5 o no, mostrar el resultado con console.log */
 
-/*let numero=parseInt(prompt("Ingrese un número"));
+/*let numero=prompt("Ingrese un número");
+while (tiene_letras(numero)) {
+    numero= prompt("ERROR, solo debe ingresar números");
+}
 
 if (numero%5 == 0 && numero%11==0) {
     console.log(numero + " es divisible por 5 y 11. " + numero + "dividido 5 es " + numero/5 ". Y " + numero" dividido 11 es " + numero/11)
@@ -60,19 +92,16 @@ if (numero%5 == 0 && numero%11==0) {
 
 /* Crear un programa que le pida al usuario dos números en un Prompt y luego muestre en un alerta el número mayor. */
 
-/*let num_uno = parseInt(prompt("Ingrese un numero"));
-
-
-while (Number.isNaN(num_uno)) {
-    num_uno = parseInt(prompt("Error, ingrese un numero"));
-    
-} 
-let num_dos = parseInt(prompt("Ingrese otro numero"));
-
-while (Number.isNaN(num_dos)) {
-    num_dos = parseInt(prompt("Error, ingrese un numero"))
+/*let num_uno = prompt("Ingrese un numero");
+while (tiene_letras(num_uno)) {
+    num_uno= prompt("ERROR, solo debe ingresar números");
 }
 
+let num_dos = parseInt(prompt("Ingrese otro numero"));
+
+while (tiene_letras(num_dos)) {
+    num_dos= prompt("ERROR, solo debe ingresar números");
+}
 
 if (num_uno>num_dos) {
     alert(num_uno + " es mayor");
@@ -82,8 +111,14 @@ if (num_uno>num_dos) {
 
 /* Crear un programa que le pida al usuario primero un números al usuario a través de un prompt y luego un segundo número para luego mostrar en un alerta el número mayor, esta vez realizar el ejercicio ocupando un if ternario. */
 
-/*let num_uno = parseInt(prompt("Ingrese un numero"));
+/*let num_uno = prompt("Ingrese un numero");
+while (tiene_letras(num_uno)) {
+    num_uno= prompt("ERROR, solo debe ingresar números");
+}
 let num_dos = parseInt(prompt("Ingrese otro numero"));
+while (tiene_letras(num_dos) {
+    num_dos= prompt("ERROR, solo debe ingresar números");
+}
 
 alert(num_uno > num_dos ? num_uno + " es mayor" : num_dos + " es mayor");*/
 
@@ -105,8 +140,17 @@ if (isNaN(primer_caracter)) {
 //TENGO DUDA CON ESTE EJERCICIO
 
 /*let ang_uno= parseFloat(prompt("Ingrese ángulo interno 1"));
+while(ang_uno <= 0) {
+    ang_uno= parseFloat(prompt("Error, ingrese un valor mayor a 0"));
+}
 let ang_dos= parseFloat(prompt("Ingrese ángulo interno 2"));
+while(ang_dos <= 0) {
+    ang_dos= parseFloat(prompt("Error, ingrese un valor mayor a 0"));
+}
 let ang_tres= parseFloat(prompt("Ingrese ángulo interno 3"));
+while(ang_tres <= 0) {
+    ang_tres= parseFloat(prompt("Error, ingrese un valor mayor a 0"));
+}
 
 alert(ang_uno + ang_dos + ang_tres == 180 ? "El triángulo es válido." : "El trángulo no es válido");*/
 
@@ -182,7 +226,10 @@ while (i<n) {
 
 /*Mostrar todos los números de 1 a N aumentando de 2 en 2 donde n lo ingresa el usuario en un prompt.*/
 
-/*let n=parseInt(prompt("Ingrese un numero"));
+/*let n=prompt("Ingrese un numero");
+while (tiene_letras(n)) {
+    n= prompt("ERROR, solo debe ingresar números");
+}
 i= 0
 
 while (i<n) {
@@ -192,7 +239,10 @@ while (i<n) {
 
 /* Mostrar todos los números de N a 1 disminuyendo de 1 en 1 donde n lo ingresa el usuario en un prompt. */
 
-/*let n=parseInt(prompt("Ingrese un numero"));
+/*let n=prompt("Ingrese un numero");
+while (tiene_letras(n)) {
+    numero= prompt("ERROR, solo debe ingresar números");
+}
 i= n
 
 while (i>=1) {
@@ -213,59 +263,62 @@ while (i<=10) {
 
 /* Pedir al usuario que ingrese un número en un prompt, hacer la suma de todos los dígitos, validar que el número ingresado no contenga letras. TENGO DUDA*/
 
-/*var numero=parseInt(prompt("Ingrese un numero"));
-x=0
-i=0
-n=0
-while (Number.isNaN(numero.charAt(x))) {
+/*var texto=prompt("ingrese un numero");*/
+
+/*while (tiene_letras(texto)) {
+    texto= prompt("ERROR, solo debe ingresar números");
+}*/
+
+/*acum=0
+
+for(x=0; x<texto.length;x++) {
+    acum+=parseInt(texto.charAt(x));
+}
+console.log(acum);*/
+
+/*let numero=parseInt(prompt)
+
+while (Number.isNaN(numero)) {
     alert("Número invalido");
     numero=parseInt(prompt("Ingrese número nuevamente"));
-}
-numero=numero.toString()
-alert("Número válido");
+}*/
 
 
-while (x<numero.length ) {
-    i += parseInt(numero.charAt(x))
-    x++
-
-}
-console.log(i);*/
-
-   
 /* Realizar la suma de todos los números pares entre N y M donde N y M los ingresa un usuario. */
 
-/*let n= parseInt(prompt("Ingrese un número"));
-let m= parseInt(prompt("Ingrese otro número"));
-
-while (n>m) {
-    m= parseInt(prompt("ERROR, ingrese un número más grande que el anterior por favor"));
+let n= prompt("Ingrese un número");
+while (tiene_letras(n)) {
+    n= prompt("ERROR, solo debe ingresar números");
 }
-    
+let m= prompt("Ingrese otro número");
+while (tiene_letras(m)) {
+    m= prompt("ERROR, solo debe ingresar números");
+}
+
+m=parseInt(m);
+n=parseInt(n);
+while (m<=n){
+    m= parseInt(prompt("ERROR, debe ser mayor al anterior numero"));
+}
 i=n
 acum=0
 
 while (i<=m) {
     if (i%2==0) {
-        
-        sumar= x + " + " + i + " = " + (acum+=i)
-        console.log(sumar);
-        
-
+        console.log(acum + "+" + i + "=" + (acum+=i));
     }
     i++
 } 
-console.log(acum);*/
+console.log(acum);
 
 //Realizar la sumatoria de los primeros N números, donde N es ingresado por el usuario. 
 
-/*let n= parseInt(prompt("Ingrese un número"));
+/*let n= prompt("Ingrese un número");
 
 acum=0
 count=1
-
-while (!Number.isInteger(n)) {
-    n= parseInt(prompt("Error, ingrese un número por favor"))
+while (tiene_letras(n)) {
+    n= prompt("ERROR, solo debe ingresar números");
 }
 
 while(count<=n) {
@@ -277,22 +330,22 @@ console.log(acum)*/
 
 //Realizar el factorial de los primeros N números.
 
-//let n= parseInt(prompt("Ingrese un número"));
+/*let n= prompt("Ingrese un número");
 
-//acum=1
-//count=1
+acum=1
+count=1
 
-/*while (!Number.isInteger(n)) {
-    n= parseInt(prompt("Error, ingrese un número por favor"))
+while (tiene_letras(n)) {
+    n= prompt("ERROR, solo debe ingresar números");
 }
 
-while(count<n) {
+while(count<=n) {
     console.log(acum + " x "+ count+ " = "+ (acum *= count));
     count++
     
 }
 
-console.log(acum)*/
+console.log(acum);*/
 
 /*let numero=parseInt(prompt("Ingrese un numero"));
 n=1
@@ -329,11 +382,10 @@ console.log(acum)*/
 //Ejercicios de divisores con while (o for)
 
 //Encontrar todos los divisores de un número.
-/*let n=parseInt(prompt("Ingrese un número"));
+/*let n=prompt("Ingrese un número");
 
-while (Number.isNaN(n)) {
-    n=parseInt(prompt("Error, ingrese un número"));
-
+while (tiene_letras(n)) {
+    n= prompt("ERROR, solo debe ingresar números");
 }
 
 count=0
@@ -349,7 +401,11 @@ console.log(divisores)*/
 
 //Determinar si un número ingresado por el usuario en un loop es primo o no, validar que el número ingresado sea mayor o igual a dos.
 
-/*let n=parseInt(prompt("Ingrese un número"));
+/*let n=prompt("Ingrese un número");
+
+while (tiene_letras(n) || n<2) {
+    n= prompt("ERROR, solo debe ingresar números, mayor o igual a 2");
+}
 
 while (Number.isNaN(n) || n<2) {
     n=parseInt(prompt("Error, ingrese un número por favor que sea mayor o igual a 2."));
@@ -374,11 +430,10 @@ if (count > 2) {
 
 //Crear un programa que determine si un número es perfecto o no, (se dice que un número es perfecto si el número es igual a sus divisores, ejemplos 6 = 1 + 2 + 3)
 
-/*let num_perfecto= parseInt(prompt("Ingrese un numero, por favor"))
+/*let num_perfecto= prompt("Ingrese un numero, por favor");
 
-while (Number.isNaN(num_perfecto)){
-    num_perfecto=parseInt(prompt("Error, ingrese un numero, por favor"))
-
+while (tiene_letras(num_perfecto)) {
+    num_perfecto= prompt("ERROR, solo debe ingresar números");
 }
 
 acum=0
@@ -395,15 +450,13 @@ acum==num_perfecto? console.log(num_perfecto + " es un numero perfecto") : conso
 
 //Generar los primeros N números primos, donde n es ingresado por el usuario.
 
+/*n= prompt("Ingrese un numero");
 
-/*n= parseInt(prompt("Ingrese un numero"));
-
-while (Number.isNaN(n)) {
-    n= parseInt(prompt("Error, ingrese un número"));
+while (tiene_letras(n)) {
+    n= prompt("ERROR, solo debe ingresar números");
 }
 
-
-function es_primo(numero) {
+function es_primo(numero) {5
     count=0
     
     for(x=1; x<=(numero); x++) {       
@@ -445,10 +498,10 @@ while ((num_primos.length)<n) {
 
 }
 
-let n= parseInt(prompt("Ingrese un numero"));
+let n= prompt("Ingrese un numero");
 
-while (Number.isNaN(n)) {
-    n=parseInt(prompt("Error, ingrese un número"));
+while (tiene_letras(n)) {
+    n= prompt("ERROR, solo debe ingresar números");
 }
 
 num_perfectos=[]
